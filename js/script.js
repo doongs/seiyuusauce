5844d7ec7c466e0f253afcab4586794abc49d97a
Gabriel Doon <doongs@dukes.jmu.edu>
2020-10-20 20:10:01 -0400

    comments

--- js/script.js
+++ js/script.js
@@ -1,7 +1,5 @@
 "use strict";
 function vaQuery(input) {
-  // Here we define our query as a multi-line string
-  // Storing it in a separate .graphql/.gql file is also possible
   var query = `
     query ($name: String) { # Define which variables will be used in the query (id)
         Staff (search: $name) { # Insert our variables into the query arguments (id) (type: ANIME is hard-coded in the query)
@@ -45,12 +43,12 @@ function vaQuery(input) {
       }
   `;
 
-  // Define our query variables and values that will be used in the query request
+  // Query Variables
   var variables = {
     name: input,
   };
 
-  // Define the config we'll need for our Api request
+  // Api request config
   var url = "https://graphql.anilist.co",
     options = {
       method: "POST",
@@ -67,8 +65,7 @@ function vaQuery(input) {
   // Make the HTTP Api request
   fetch(url, options).then(handleResponse).then(handleData).catch(handleError);
 
-  
-
+  // Use the data recieved
   function handleResponse(response) {
     return response.json().then(function (json) {
       return response.ok ? json : Promise.reject(json);
@@ -80,67 +77,67 @@ function vaQuery(input) {
     document.querySelector(`#top`).appendChild(createVACard(data.data.Staff));
   }
 
+  // On Error
   function handleError(error) {
     alert("Search failed");
     console.error(error);
   }
 
+  // Helper method to create the VA Card HTML
   function createVACard(staff) {
-      let card = document.createElement("div");
-      card.classList.add("card", "mr-5", "ml-5");
+    let card = document.createElement("div");
+    card.classList.add("card", "mr-5", "ml-5");
 
-      let vaImageAnchor = document.createElement("a");
-      vaImageAnchor.href = staff.siteUrl;
-      vaImageAnchor.target = "_blank";
+    let vaImageAnchor = document.createElement("a");
+    vaImageAnchor.href = staff.siteUrl;
+    vaImageAnchor.target = "_blank";
 
-      let vaImage = document.createElement("img");
-      vaImage.classList.add("card-img");
-      vaImage.id = staff.name.full.replace(/\s+/g, '');
-      vaImage.src = staff.image.large;
+    let vaImage = document.createElement("img");
+    vaImage.classList.add("card-img");
+    vaImage.id = staff.name.full.replace(/\s+/g, "");
+    vaImage.src = staff.image.large;
 
-      vaImageAnchor.appendChild(vaImage);
+    vaImageAnchor.appendChild(vaImage);
 
-      let cardBody = document.createElement("div");
-      cardBody.classList.add("card-body");
+    let cardBody = document.createElement("div");
+    cardBody.classList.add("card-body");
 
-      let vaNameAnchor = document.createElement("a");
-      vaNameAnchor.href = staff.siteUrl;
+    let vaNameAnchor = document.createElement("a");
+    vaNameAnchor.href = staff.siteUrl;
 
-      let vaNameText = document.createElement("h5");
-      vaNameText.classList.add("card-title");
-      vaNameText.textContent = staff.name.full;
+    let vaNameText = document.createElement("h5");
+    vaNameText.classList.add("card-title");
+    vaNameText.textContent = staff.name.full;
 
-      let vaNameNativeAnchor = document.createElement("a");
-      vaNameNativeAnchor.href = staff.siteUrl;
+    let vaNameNativeAnchor = document.createElement("a");
+    vaNameNativeAnchor.href = staff.siteUrl;
 
-      let vaNameNativeText = document.createElement("small");
-      vaNameNativeText.classList.add("text-muted");
-      vaNameNativeText.textContent = staff.name.native;
-      console.log(staff.name.native);
+    let vaNameNativeText = document.createElement("small");
+    vaNameNativeText.classList.add("text-muted");
+    vaNameNativeText.textContent = staff.name.native;
+    console.log(staff.name.native);
 
-      vaNameAnchor.appendChild(vaNameText);
-      vaNameAnchor.target = "_blank";
+    vaNameAnchor.appendChild(vaNameText);
+    vaNameAnchor.target = "_blank";
 
-      vaNameNativeAnchor.appendChild(vaNameNativeText);
-      vaNameNativeAnchor.target = "_blank";
+    vaNameNativeAnchor.appendChild(vaNameNativeText);
+    vaNameNativeAnchor.target = "_blank";
 
-      cardBody.appendChild(vaNameAnchor);
-      cardBody.appendChild(vaNameNativeAnchor);
-      
-      card.appendChild(vaImageAnchor);
-      card.appendChild(cardBody);
+    cardBody.appendChild(vaNameAnchor);
+    cardBody.appendChild(vaNameNativeAnchor);
 
-      return card;
+    card.appendChild(vaImageAnchor);
+    card.appendChild(cardBody);
+
+    return card;
   }
 
-  (function() {
-  })();
-
+  (function () {})();
 }
 
 function userQuery(input) {
-    var query = `
-    query ($name: String) { # Define which variables will be used in the query (id)
+  var query = `
+    query ($name: String) { 
         User(name: $name) {
           id
           siteUrl
@@ -153,12 +150,12 @@ function userQuery(input) {
       }
   `;
 
-  // Define our query variables and values that will be used in the query request
+  // Query Variables
   var variables = {
     name: input,
   };
 
-  // Define the config we'll need for our Api request
+  // Api request config
   var url = "https://graphql.anilist.co",
     options = {
       method: "POST",
@@ -175,27 +172,29 @@ function userQuery(input) {
   // Make the HTTP Api request
   fetch(url, options).then(handleResponse).then(handleData).catch(handleError);
 
-  
-
+  // Handle API response
   function handleResponse(response) {
     return response.json().then(function (json) {
       return response.ok ? json : Promise.reject(json);
     });
   }
 
+  // Use the data recieved
   function handleData(data) {
     console.log(data.data.User);
-    document.querySelector(`#profile-picture`).src = data.data.User.avatar.large;
+    document.querySelector(`#profile-picture`).src =
+      data.data.User.avatar.large;
     document.querySelector(`#profile-name`).textContent = data.data.User.name;
-    document.querySelector(`#profile-picture-link`).href = data.data.User.siteUrl;
+    document.querySelector(`#profile-picture-link`).href =
+      data.data.User.siteUrl;
     document.querySelector(`#profile-name-link`).href = data.data.User.siteUrl;
   }
 
+  // On Error
   function handleError(error) {
     alert("Search failed");
     console.error(error);
   }
-  
 }
 
 /*
